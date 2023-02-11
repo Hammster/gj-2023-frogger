59ac043d7639823fc577cd0a0474fed8e8fb20cb
Hans Koch <mail@hans-koch.me>
2023-02-11 14:47:18 +0100

    Added frog movement and map movement, changed grass

--- Frog.gd
+++ Frog.gd
@@ -1,21 +1,32 @@
 extends Sprite
 
+var spiteSize = 16
+var bounding_x = spiteSize * 15
+var bounding_y = spiteSize * 8
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.position.x = 0
-	self.position.y = 0
+	self.position.x = bounding_x / 2 - 8
+	self.position.y = bounding_y
 	pass # Replace with function body.
 
 func _input(event):
-	if event.is_action_pressed("ui_right"):
-		print("R");
+	# Input Handling
+	if event.is_action_pressed("ui_right") and self.position.x < bounding_x:
 		position.x += 16
-	if event.is_action_pressed("ui_left"):
-		print("L");
+	if event.is_action_pressed("ui_left") and self.position.x > 0:
 		position.x -= 16
-	if event.is_action_pressed("ui_up"):
-		print("U");
+	if event.is_action_pressed("ui_up") and self.position.y > 0:
 		position.y -= 16
-	if event.is_action_pressed("ui_down"):
-		print("D");
+		self.flip_v = false
+	if event.is_action_pressed("ui_down") and self.position.y < bounding_y:
 		position.y += 16
+		self.flip_v = true
+	
+	# Screen Movement
+	if self.position.y == 16:
+		get_parent().get_node("Grass").position.y += bounding_y - spiteSize
+		self.position.y = bounding_y
+		
+	# Collision Check
+	
---
+++ Grass.gd
@@ -0,0 +1,21 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _draw():
+	for x in range(16):
+		for y in range(256):
+			draw_rect(Rect2(x * 16, y * 16, x * 16 + 16, y * 16 + 16), "#0000FF", false)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
