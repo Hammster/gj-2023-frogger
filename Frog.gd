6d7a0d86e5d768a236742fff73e77bcd144b44c7
Hans Koch <mail@hans-koch.me>
2023-02-11 15:18:50 +0100

    Change frog movement tile wise

--- Frog.gd
+++ Frog.gd
@@ -3,11 +3,15 @@ extends Sprite
 var spiteSize = 16
 var bounding_x = spiteSize * 15
 var bounding_y = spiteSize * 8
+var grass = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.position.x = bounding_x / 2 - 8
-	self.position.y = bounding_y
+	self.position.y = bounding_y - spiteSize
+	grass = get_parent().get_node("Grass")
+	grass.position.y = 160 - grass.region_rect.size.y
+	print(grass.region_rect.size.y)
 	pass # Replace with function body.
 
 func _input(event):
@@ -17,16 +21,18 @@ func _input(event):
 	if event.is_action_pressed("ui_left") and self.position.x > 0:
 		position.x -= 16
 	if event.is_action_pressed("ui_up") and self.position.y > 0:
-		position.y -= 16
+		#position.y -= 16
+		grass.position.y += 16
 		self.flip_v = false
 	if event.is_action_pressed("ui_down") and self.position.y < bounding_y:
-		position.y += 16
+		#position.y += 16
+		grass.position.y -= 16
 		self.flip_v = true
 	
-	# Screen Movement
-	if self.position.y == 16:
-		get_parent().get_node("Grass").position.y += bounding_y - spiteSize
-		self.position.y = bounding_y
-		
+
+	#if self.position.y == bounding_y - 16:
+	#	grass.position.y -= bounding_y - spiteSize
+	#	self.position.y = 0
+	
 	# Collision Check
 	
